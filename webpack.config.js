4fd3898cf346d463db1dd4a5fc1b3efc9f0b95b4
sebastian valdez <valdez.sebastian4@gmail.com>
2020-10-17 16:05:08 -0700

    Git battle react app

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: ['style-loader', 'css-loader'] },
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({ verbose: true}),
+    new CopyPlugin({ patterns: [{ from : '_redirects' }] }),
+    new HtmlWebpackPlugin({ template: './app/index.html'})
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devServer: {
+    historyApiFallback: true
+  }
+}
